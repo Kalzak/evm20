9949126448898fe100f9834c1f33fe3464c1bf7b
Kalzak <kalzak.dev@protonmail.com>
2023-02-26 15:57:47 +0800

    Implemented fuzz tests

--- test/evm20.t.sol
+++ test/evm20.t.sol
@@ -9,6 +9,9 @@ import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 contract CounterTest is Test {
     IERC20 private evm20;
 
+    // This is the address of the huff deployer, they own all tokens at deployment
+    address constant private owner = address(0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f);
+
     address private alice = address(0x1111);
     address private bob = address(0x2222);
 
@@ -18,55 +21,96 @@ contract CounterTest is Test {
 
     function testTotalSupply() public {
         uint256 totalSupply = evm20.totalSupply();
-        assertEq(totalSupply, 1000);
+        assertEq(totalSupply, type(uint256).max);
     }
 
-    function testBalanceOfKnown() public {
-        uint256 balance = evm20.balanceOf(alice);
-        assertEq(balance, 500);
+    function testBalanceOfEmpty(address addr) public {
+	vm.assume(addr != owner);
+
+	uint256 balance = evm20.balanceOf(addr);
+	assertEq(balance, 0);
     }
 
-    function testBalanceOfUnknown() public {
-        uint256 balance = evm20.balanceOf(address(0));
-        assertEq(balance, 0);
+    function testBalanceOfAtDeployment() public {
+        uint256 balance = evm20.balanceOf(owner);
+	assertEq(balance, type(uint256).max);
     }
 
-    function testTransfer() public {
-        vm.prank(alice);
-        bool transferReturn = evm20.transfer(bob, 250);
-	uint256 aliceNewBalance = evm20.balanceOf(alice);
-	uint256 bobNewBalance = evm20.balanceOf(bob);
-	assertEq(aliceNewBalance, 250);
-	assertEq(bobNewBalance, 750);
-	assertEq(transferReturn, true);
+    function testTransferReducesBalance(address sender, uint256 amount) public {
+	uint256 balanceBefore = evm20.balanceOf(owner);
+        vm.prank(owner);
+	evm20.transfer(sender, amount);
+	uint256 balanceAfter = evm20.balanceOf(owner);
+	assertEq(balanceBefore, balanceAfter + amount);
     }
 
-    function testFailsTransferTooMuch() public {
-        vm.prank(alice);
-        evm20.transfer(bob, 1250);
+    function testTransferIncreasesBalance(address recipient, uint256 amount) public {
+        vm.assume(recipient != owner);
+	uint256 balanceBefore = evm20.balanceOf(recipient);
+        vm.prank(owner);
+	evm20.transfer(recipient, amount);
+	uint256 balanceAfter = evm20.balanceOf(recipient);
+	assertEq(balanceBefore, balanceAfter - amount);
     }
 
-    function testAllowance() public {
-        uint256 allowanceBefore = evm20.allowance(alice, bob);
-	vm.prank(alice);
-	bool success = evm20.approve(bob, 100);
-        uint256 allowanceAfter = evm20.allowance(alice, bob);
-	assertEq(allowanceBefore, 0);
-	assertEq(allowanceAfter, 100);
-	assertEq(success, true);
+    function testTransferEntireBalance(address sender, address recipient, uint256 amount) public {
+        vm.assume(sender != owner);
+	vm.assume(recipient != owner);
+	vm.assume(sender != recipient);
+
+	vm.prank(owner);
+	evm20.transfer(sender, amount);
+	vm.prank(sender);
+	evm20.transfer(recipient, amount);
+	uint256 balance = evm20.balanceOf(sender);
+	assertEq(balance, 0);
     }
 
-    function testTransferFrom() public {
-        vm.prank(alice);
-	evm20.approve(bob, 100);
-	vm.prank(bob);
-	bool success = evm20.transferFrom(alice, bob, 100);
-        uint256 allowanceAfter = evm20.allowance(alice, bob);
-	uint256 aliceNewBalance = evm20.balanceOf(alice);
-	uint256 bobNewBalance = evm20.balanceOf(bob);
-	assertEq(aliceNewBalance, 400, "sender balance didnt decrease");
-	assertEq(bobNewBalance, 600, "recipient balance didnt increase");
-	assertEq(allowanceAfter, 0, "allowance did not decrease");
-	assertEq(success, true);
+    function testTransferSelf(address sender, uint256 startAmount, uint256 transferAmount) public {
+        vm.assume(startAmount >= transferAmount);
+	vm.assume(sender != owner);
+
+	vm.prank(owner);
+	evm20.transfer(sender, startAmount);
+	uint256 balanceBefore = evm20.balanceOf(sender);
+	vm.prank(sender);
+	evm20.transfer(sender, transferAmount);
+	uint256 balanceAfter = evm20.balanceOf(sender);
+	assertEq(balanceBefore, balanceAfter);
+    }
+
+    function testFailTransferMoreThanBalance(address sender, address recipient, uint256 startAmount, uint256 transferAmount) public {
+        vm.assume(sender != owner);
+	vm.assume(recipient != owner);
+        vm.assume(transferAmount > startAmount);
+
+	vm.prank(owner);
+	evm20.transfer(sender, startAmount);
+	vm.prank(sender);
+	evm20.transfer(recipient, transferAmount);
+    }
+
+    function testApprove(address sender, address recipient, uint256 amount) public {
+	vm.prank(sender);
+	evm20.approve(recipient, amount);
+	uint256 approval = evm20.allowance(sender, recipient);
+	assertEq(approval, amount);
+    }
+
+    function testApproveClear(address sender, address recipient, uint256 amount) public {
+	vm.prank(sender);
+	evm20.approve(recipient, amount);
+	vm.prank(sender);
+	evm20.approve(recipient, 0);
+	uint256 approval = evm20.allowance(sender, recipient);
+	assertEq(approval, 0);
+    }
+
+    function testTransferFromReducesBalance(address recipient, uint256 amount) public {
+        uint256 balanceBefore = evm20.balanceOf(owner);
+        vm.prank(owner);
+	evm20.transferFrom(owner, recipient, amount);
+        uint256 balanceAfter = evm20.balanceOf(owner);
+        assertEq(balanceBefore, balanceAfter + amount);
     }
 }
